4028474629e2a27062592c0587ef96e0913cb199
DaniilTsarev777 <daniilwestsiberian2@gmail.com>
2024-12-04 21:37:53 +0500

    Add files via upload

---
+++ Tyuiu.TsarevDI.Sprint6.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.TsarevDI.Sprint6.Task2.V8.Lib;
+namespace Tyuiu.TsarevDI.Sprint6.Task2.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
